6cbbfd57f0a0f66231bacfcfe6421eb4ba1dd879
chchibiru <akiyama.sys.n.ctrl@gmail.com>
2024-03-28 15:51:33 +0900

    fix tncc version

--- create_env.sh
+++ create_env.sh
@@ -8,7 +8,7 @@ conda install -y pytorch==2.1.0 pytorch-cuda=11.8 -c pytorch -c nvidia
 # install tiny-cuda-nn
 export PATH="/usr/local/cuda/bin:$PATH"
 export LIBRARY_PATH="/usr/local/cuda/lib64/stubs:$LIBRARY_PATH"
-pip install git+https://github.com/NVlabs/tiny-cuda-nn/@v1.6#subdirectory=bindings/torch
+pip install git+https://github.com/NVlabs/tiny-cuda-nn/@2ec562e853e6f482b5d09168705205f46358fb39#subdirectory=bindings/torch
 
 pip install -e ./third_parties/nerfacc-0.3.5/nerfacc-0.3.5/
 pip install opencv-python==4.8.1.78 trimesh==3.23.5 open3d==0.17 pyvista==0.42.3 scipy==1.10.1 scikit-image==0.21.0 pyhocon==0.3.59 pyexr==0.3.10 tensorboard==2.14.0 icecream==2.1.3 PyMCubes==0.1.4 pyembree==0.2.11
